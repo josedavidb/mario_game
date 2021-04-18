d21c7e8653fd22f50dcb9e5f860e4c1713e585fe
WGraterol64 <solo.wilfredo64@gmail.com>
2021-04-18 19:33:35 -0400

    Added ame final animations

--- Assets/Scripts/AmeMovement.cs
+++ Assets/Scripts/AmeMovement.cs
@@ -7,6 +7,7 @@ public class AmeMovement : MonoBehaviour
 {
     public float Speed;
     public float JumpForce;
+    public float RollForce;
 
     private Animator Animator;
     private Rigidbody2D Rigidbody2D;
@@ -29,22 +30,22 @@ public class AmeMovement : MonoBehaviour
         {
             if (Ground)
             {
-                Jump();
+                StartCoroutine(Jump());
                 Ground = false;
             }
         }
-        else if (Input.GetKeyDown(KeyCode.G))
-        {
-            Rigidbody2D.AddForce(Vector2.down * JumpForce);
-        }
-        else if (Input.GetKeyDown(KeyCode.R))
+        //else if (Input.GetKeyDown(KeyCode.G))
+        //{
+        //    Rigidbody2D.AddForce(Vector2.down * JumpForce);
+        //}
+        else if (Input.GetKeyDown(KeyCode.R) && Ground)
         {
             Animator.SetTrigger("Attack");
             Roll();
         }
         else if (Input.GetKeyDown(KeyCode.T))
         {
-            Die();
+            StartCoroutine(Die());
         }
         Animator.SetBool("Jump", !Ground);
     }
@@ -65,14 +66,15 @@ public class AmeMovement : MonoBehaviour
         }
     }
 
-    private void Jump()
-    {
-        Rigidbody2D.AddForce(Vector2.up * JumpForce);
-    }
+    //private void Jump()
+    //{
+    //    Rigidbody2D.AddForce(Vector2.up * JumpForce);
+    //}
 
     private void Roll()
     {
-        Rigidbody2D.AddForce(Vector2.right * 10);
+        Rigidbody2D.velocity = new Vector2(Horizontal * Speed+ RollForce, Rigidbody2D.velocity.y);
+        Animator.SetFloat("Speed", Mathf.Abs(Horizontal * Speed + RollForce));
     }
 
     public void DieAnimation()
@@ -82,8 +84,49 @@ public class AmeMovement : MonoBehaviour
 
     }
 
-    public void Die()
+    public IEnumerator Die()
     {
+        Animator.SetTrigger("Dead");
+        Rigidbody2D.AddForce(Vector2.up * JumpForce);
+        GetComponent<CapsuleCollider2D>().enabled = false;
+
+        float counter = 0;
+        float waitTime = 2.0f; //Animator.GetCurrentAnimatorStateInfo(0).length;
+
+        //Now, Wait until the current state is done playing
+        while (counter < (waitTime))
+        {
+            counter += Time.deltaTime;
+            yield return null;
+        }
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public IEnumerator Jump()
+    {
+        Animator.Play("Ame_bounce");
+        //Animator.CrossFadeInFixedTime("Ame_bounce", 0.6f);
+
+        //Wait until we enter the current state
+        //while (Animator.GetCurrentAnimatorStateInfo(0).IsName("Ame_bounce"))
+        //{
+        //    yield return null;
+        //}
+
+        float counter = 0;
+        float waitTime = Animator.GetCurrentAnimatorStateInfo(0).length;
+        
+        //Now, Wait until the current state is done playing
+        while (counter < (waitTime))
+        {
+            counter += Time.deltaTime;
+            yield return null;
+        }
+
+        Animator.Play("Ame_jump");
+
+        Rigidbody2D.AddForce(Vector2.up * JumpForce);
+
     }
 }
